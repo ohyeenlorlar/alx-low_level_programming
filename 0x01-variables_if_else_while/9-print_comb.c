13a5a9a45a356b69961d6b5144a51904e7399905
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-10-16 15:29:01 -0700

    commit for comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**\m*main - main function
+*Return: 0
+**/
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+if (a = '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
