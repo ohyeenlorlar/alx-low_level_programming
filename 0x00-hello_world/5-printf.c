dfe875983ae29c58faff7db8410b9b2ce95d26d4
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-10-17 02:12:01 -0700

    commit for printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,10 @@
 
 /**
 *main - Entry point
-*
-*Return: Always 0(success)
+*Return: 0
 */
 int main(void)
 {
-printf ("with proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
