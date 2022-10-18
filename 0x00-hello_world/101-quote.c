5415bc84acbd14f97c51116fb9d026db276a611b
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-10-18 09:31:06 -0700

    .

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 
 /**
-*main - prints "and that piece of art is useful" - Dora Korpar,2015 - 10 - 19"
-*followed by a new line, to standard error.
-*Return: Always 1
+*main - Entry point
+*Description: prints "and that piece of art is useful" - Dora Korpar, 25-10-19"
+*			followed by a new line, to standard error.
+*Return: Always 1.
 */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n",
+		"and the piece of art is useful" - Dora Korpar, 2015-10-19\n",
 		59);
 
-eturn (1);
+return (1);
 }
