4db7824045cc79c1989ee3aa68db7f89338f071d
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-10-25 04:51:15 -0700

    commit for puts2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,8 @@
 #include <string.h>
 
 /**
-*print_rev - a function that prints a string in reverse
-*@s: str input
-Return: string in reverse
+*print_rev - a function that prints a string in reverse\*@s: str input
+*Return: string in reverse
 */
 void print_rev(char *s)
 {
@@ -13,5 +12,6 @@ void print_rev(char *s)
 	len = strlen(s);
 	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
+
 	_putchar('\n');
 }
