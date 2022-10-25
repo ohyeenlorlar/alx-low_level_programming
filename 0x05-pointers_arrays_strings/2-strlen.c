f20b64e49b78ccca787e4eb802ffdb4b83be878e
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-10-25 02:59:14 -0700

    commit for strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,16 @@
 #include "main.h"
 
 /**
-*_strlen - a function that returns the length of a string
+*_strlen - a function that prints the length of a string
 *@s: char input
 *Return: length of the input string
 */
 int _strlen(char *s)
 {
-	int len;
-
-
-	while(*s != '\0')
+	int len = 0;
+	while (s[len] != '\0')
 	{
-		len = len + 1;
-		s = s + 1;
+		len++
 	}
 	return (len);
 }
