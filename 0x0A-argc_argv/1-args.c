876716f4d051fa583de395198d9240aed7f01956
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-11-04 06:55:35 -0700

    commit for args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,22 +2,14 @@
 #include <stdio.h>
 
 /**
-*main - prints the number of arguments passed into the main
+*main- prints the number of arguments
 *@argc: number of command line arguments
 *@argv: array name
 *Return: 0
 */
 int main(int argc, char *argv[])
 {
-	int i;
-
-	if (argc == 1)
-		ptintf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++);
-
-		printf("%d\n", i - 1);
-	}
+	(void) argv;/*ignore argv*/
+	printf("%i\n", argc - 1);
 	return (0);
 }
