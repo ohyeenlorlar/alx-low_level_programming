4bba54907f544b9858027e799afeb0b8df3557cc
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-11-04 05:17:12 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*@argc: Number of command line arguments
+*@argv: Array name
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
