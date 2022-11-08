0ec3a652d5eb14fbccbe59416d23490405e991bc
ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2022-11-08 00:43:08 -0800

    header file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - prints the name of the program
+ *@argc: counts arguments
+ *@argv: arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
