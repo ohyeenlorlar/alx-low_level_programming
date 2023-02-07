4d20e22492e56efe9bd22d8c3a8b2e25d7381d1a
Ohyeenlorlar <oyinlolaelizabeth2017@gmail.com>
2023-02-07 10:43:05 -0800

    commit for this task

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*_strlen - a function that prints the length of a string
+*@s: char input
+*Return: length of the input string
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
